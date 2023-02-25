bc27e0ee4399dabbc21c6239fc786631113b5ce6
ayoayomide <ayoayomide124@gmail.com>
2023-02-25 18:54:09 +0100

    remain sound

--- src/js/boost.js
+++ src/js/boost.js
@@ -1,10 +1,25 @@
 AFRAME.registerComponent('boost', {
     init: function () {
-        console.log(startGame);
+        this.maxSpeed = 2;
+        this.acceleration = 0.01;
+        this.speed = 0.01;
+
+        this.move = (data) => {
+            if (this.speed < this.maxSpeed) {
+                this.speed += (this.acceleration / 100);
+                return this.speed;
+            } else {
+                return this.speed;
+            }
+        }
     },
     tick: function (time, deltaTime) {
         if (startGame) {
-            this.el.object3D.position.y += 0.03;
+            this.el.object3D.position.y += this.move();
+        }
+        if (restartGame) {
+            this.speed = 0.01;
+            this.el.object3D.position.y = 7;
         }
     }
 })
